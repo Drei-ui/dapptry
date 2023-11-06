7033d396b1b893aa02d7ff152f213518bbeca8ca
Drei-ui <73028182+Drei-ui@users.noreply.github.com>
2023-11-06 16:21:47 +0800

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -6,7 +6,7 @@ const timerMessage = document.querySelector('#timerMessage');
 const mainBoard = document.querySelector('#mainBoard');
 const voteForm = document.querySelector('#voteForm');
 const vote = document.querySelector('#vote');
-const votebtn = document.querySelector('#sendVote');
+const voteBtn = document.querySelector('#sendVote');
 const showResultContainer = document.querySelector('#showResultContainer');
 const showResult = document.querySelector('#showResult');
 const result = document.querySelector('#result');
@@ -18,290 +18,292 @@ const candidate = document.querySelector('#candidate');
 const addTheCandidate = document.querySelector('#addTheCandidate');
 
 //ethers configuration
-const contractAddress = '0x07C152Ce4109faA901b4BD00Dd50aC5da5ECc712';
-const contractABI =  [
-    {
-      "inputs": [],
-      "stateMutability": "nonpayable",
-      "type": "constructor"
-    },
-    {
-      "inputs": [],
-      "name": "electionTimer",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "admin",
-      "outputs": [
-        {
-          "internalType": "address",
-          "name": "",
-          "type": "address"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "name": "candidates",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "id",
-          "type": "uint256"
-        },
-        {
-          "internalType": "string",
-          "name": "name",
-          "type": "string"
-        },
-        {
-          "internalType": "uint256",
-          "name": "numVotes",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "checkElectionPeriod",
-      "outputs": [
-        {
-          "internalType": "bool",
-          "name": "",
-          "type": "bool"
-        }
-      ],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "string",
-          "name": "_name",
-          "type": "string"
-        }
-      ],
-      "name": "createCandidate",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "electionStarted",
-      "outputs": [
-        {
-          "internalType": "bool",
-          "name": "",
-          "type": "bool"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "getVotes",
-      "outputs": [
-        {
-          "components": [
-            {
-              "internalType": "uint256",
-              "name": "id",
-              "type": "uint256"
-            },
-            {
-              "internalType": "string",
-              "name": "name",
-              "type": "string"
-            },
-            {
-              "internalType": "uint256",
-              "name": "numVotes",
-              "type": "uint256"
-            }
-          ],
-          "internalType": "struct EVS.Candidate[]",
-          "name": "",
-          "type": "tuple[]"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "resetVoterStatus",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "string[]",
-          "name": "_candidates",
-          "type": "string[]"
-        },
-        {
-          "internalType": "uint256",
-          "name": "_votingLength",
-          "type": "uint256"
-        }
-      ],
-      "name": "startElection",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "vEnd",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "vStart",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "_id",
-          "type": "uint256"
-        }
-      ],
-      "name": "voteTo",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "name": "voterList",
-      "outputs": [
-        {
-          "internalType": "address",
-          "name": "",
-          "type": "address"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "_voter",
-          "type": "address"
-        }
-      ],
-      "name": "voterStatus",
-      "outputs": [
-        {
-          "internalType": "bool",
-          "name": "",
-          "type": "bool"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "",
-          "type": "address"
-        }
-      ],
-      "name": "voters",
-      "outputs": [
-        {
-          "internalType": "bool",
-          "name": "",
-          "type": "bool"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    }
-  ]
+const contractAddress = '0xC05532E8F400D7A6288a94304E11d2675AD0FaF3';
+const contractABI =[
+  {
+    "inputs": [],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_name",
+        "type": "string"
+      }
+    ],
+    "name": "addCandidate",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "candidates",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "id",
+        "type": "uint256"
+      },
+      {
+        "internalType": "string",
+        "name": "name",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "numberOfVotes",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "checkElectionPeriod",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "electionStarted",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "electionTimer",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "listOfVoters",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "resetAllVoterStatus",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "retrieveVotes",
+    "outputs": [
+      {
+        "components": [
+          {
+            "internalType": "uint256",
+            "name": "id",
+            "type": "uint256"
+          },
+          {
+            "internalType": "string",
+            "name": "name",
+            "type": "string"
+          },
+          {
+            "internalType": "uint256",
+            "name": "numberOfVotes",
+            "type": "uint256"
+          }
+        ],
+        "internalType": "struct Voting.Candidate[]",
+        "name": "",
+        "type": "tuple[]"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string[]",
+        "name": "_candidates",
+        "type": "string[]"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_votingDuration",
+        "type": "uint256"
+      }
+    ],
+    "name": "startElection",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_id",
+        "type": "uint256"
+      }
+    ],
+    "name": "voteTo",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_voter",
+        "type": "address"
+      }
+    ],
+    "name": "voterStatus",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "voters",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "votingEnd",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "votingStart",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+]
+
 let contract;
 let signer;
 
 const provider = new ethers.providers.Web3Provider(window.ethereum,80001);
 provider.send("eth_requestAccounts",[]).then(() =>{
-  provider.listAccounts().then((accounts) => {
-    signer = provider.getSigner(accounts[0])
-    contract = new ethers.Contract(contractAddress,contractABI,signer)
-  });
+    provider.listAccounts().then((accounts) => {
+      signer = provider.getSigner(accounts[0])
+      contract = new ethers.Contract(contractAddress,contractABI,signer)
+    });
 });
 
 //Necessary Functions
 const getAllCandidates = async function() {
     if(document.getElementById("candidateBoard")){
         document.getElementById("candidateBoard").remove();
-    }
-    let board = document.createElement("table");
-    board.id = "candidateBoard";
-    mainBoard.appendChild(board);
+      }
 
-    let tableHeader = document.createElement("tr");
-    tableHeader.innerHTML = `<th>ID No.</th>
-                            <th>Candidate</th>`;
-    board.appendChild(tableHeader);
+      let board = document.createElement("table");
+      board.id = "candidateBoard";
+      mainBoard.appendChild(board);
 
-    let candidates = await contract.retrieveVotes();
-    for(let i = 0; i < candidates.length; i++){
-        let candidate = document.createElement("tr");
-        candidate.innerHTML = ` <td>${parseInt(candidates[i][0])}</td>
-                            <td>${candidates[i][1]}</td>`;
-        board.appendChild(candidate);
+      let tableHeader = document.createElement("tr");
+      tableHeader.innerHTML = `<th>ID No.</th>
+                              <th>Candidate</th>`;
+      board.appendChild(tableHeader);
+
+      let candidates = await contract.retrieveVotes();
+      for(let i = 0; i < candidates.length; i++){
+          let candidate = document.createElement("tr");
+          candidate.innerHTML = ` <td>${parseInt(candidates[i][0])}</td>
+                              <td>${candidates[i][1]}</td>`;
+          board.appendChild(candidate);
     }
 }
 
@@ -318,16 +320,16 @@ const getResult = async function(){
 
     let tableHeader = document.createElement("tr");
     tableHeader.innerHTML = `<td>2</td>
-                            <td>Candidate 2</td>
-                            <td>Vote Count</td>`;
+                              <td>Candidate 2</td>
+                              <td>Vote Count</td>`;
     resultBoard.appendChild(tableHeader);
 
     let candidates = await contract.retrieveVotes();
     for(let i = 0; i < candidates.length; i++){
         let candidate = document.createElement("tr");
         candidate.innerHTML = ` <td>${parseInt(candidates[i][0])}</td>
-                            <td>${candidates[i][1]}</td>
-                            <td>${parseInt(candidates[i][2])}</td>`;
+                              <td>${candidates[i][1]}</td>
+                              <td>${parseInt(candidates[i][2])}</td>`;
         resultBoard.appendChild(candidate);
     }
 }
@@ -340,14 +342,15 @@ const refreshPage = function() {
             timerMessage.innerHTML = `<span id="time">${time}</span> second/s left`;
             voteForm.style.display = 'flex';
             showResultContainer.style.display = 'none';
-        } else{
+        } 
+        else{
             timerMessage.textContent = "No election yet or election already ended";
             voteForm.style.display = "none";
             showResultContainer.style.display = 'block';
-        }
+          }
     }, 10000);
-    setInterval(async() => {
-        getAllCandidates();
+      setInterval(async() => {
+          getAllCandidates();
     }, 10000)
 }
 
@@ -358,10 +361,10 @@ const sendVote = async function(){
 
 const startElection = async function(){
     if(!candidates.value){
-        alert('list of candidates is empty.');
+      alert('list of candidates is empty.');
     }
     if(!electionDuration.value){
-        alert("Please set election duration.");
+      alert('Please set election duration.');
     }
     //organize
     const _candidates =candidates.value.split(",");
@@ -379,7 +382,7 @@ const startElection = async function(){
 
 const addCandidate = async function() {
     if(!candidate.value){
-        alert('please provide the candidate name first');
+      alert('please provide the candidate name first');
     }
 
     await contract.addCandidate(candidate.value);
@@ -388,34 +391,36 @@ const addCandidate = async function() {
 }
 const getAccount = async function(){
   const ethAccounts = await provider.send("eth_requestAccounts", []).then(()=>{
-      provider.listAccounts().then((accounts) => {
-          signer = provider.getSigner(accounts[0]);
-          contract = new ethers.Contract(contractAddress, contractABI, signer)
-      });
+    provider.listAccounts().then((accounts) => {
+        signer = provider.getSigner(accounts[0]);
+        contract = new ethers.Contract(contractAddress, contractABI, signer)
+    });
   });
 
-  connectWalletBtn.textContent = signer.address.slice(0,10) + "...";
+  connectWalletBtn.textContent = signer._address.slice(0,10) + "...";
   connectWalletMsg.textContent = "You are currently connected.";
   connectWalletBtn.disabled = true;
 
   let owner = await contract.owner();
   if(owner == signer._address){
-      admin.style.display = "flex";
+    admin.style.display = "flex";
 
-      let time = await contract.electionTimer();
-      if(time == 0){
-          contract.checkElectionPeriod();
-      }
+    let time = await contract.electionTimer();
+    if(time == 0){
+        contract.checkElectionPeriod();
+    }
   }
   votingStation.style.display = "block";
+
   refreshPage();
   getAllCandidates();
 };
 
-//event listeners
-connectWalletBtn.addEventListener('click',getAccount);
-showResult.addEventListener('click',getResult);
-votebtn.addEventListener('click',sendVote);
-addTheCandidate.addEventListener('click',addCandidate);
-startAnElection.addEventListener('click',startElection);
+
+  //event listeners
+connectWalletBtn.addEventListener('click', getAccount);
+showResult.addEventListener('click', getResult);
+voteBtn.addEventListener('click', sendVote);
+addTheCandidate.addEventListener('click', addCandidate);
+startAnElection.addEventListener('click', startElection);
 
